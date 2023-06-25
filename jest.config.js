d7a051d72bb94a15c8f030043e51a6b0d80996d4
Ike Gabriel Yuson <iggyyuson09@gmail.com>
2023-06-26 00:42:04 +0800

    add unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,41 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+  moduleNameMapper: {
+    '^/opt/(.*)$': '<rootDir>/src/layers/nodeLibs/$1',
+  },
+  moduleDirectories: [
+    'node_modules',
+    'src/layers/nodeLibs/nodejs/node_modules',
+  ],
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+      tsconfig: {
+        target: 'ES2018',
+        module: 'commonjs',
+        lib: ['es2018'],
+        declaration: true,
+        strict: true,
+        noImplicitAny: true,
+        strictNullChecks: true,
+        noImplicitThis: true,
+        alwaysStrict: true,
+        noUnusedLocals: false,
+        noUnusedParameters: false,
+        noImplicitReturns: true,
+        noFallthroughCasesInSwitch: false,
+        inlineSourceMap: true,
+        inlineSources: true,
+        experimentalDecorators: true,
+        strictPropertyInitialization: false,
+        isolatedModules: false,
+        baseUrl: '.',
+        outDir: './dist',
+        paths: {
+          '/opt/*': ['src/layers/nodeLibs/*'],
+        },
+      },
+    },
+  },
 };
